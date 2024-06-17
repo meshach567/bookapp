ed637d09d35d7f0aa3c551221328d5ff0230d6a1
Okiemute Gold <goldokiemute@gmail.com>
2024-06-17 10:17:27 +0100

    make changes to repo

--- server.js
+++ server.js
@@ -4,19 +4,27 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
 const expressLayouts = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000;
 const indexRouter = require('./routes/index')
+const authorRouter = require("./routes/author")
+const bookRouter = require('./routes/books')
 
 const MONGOURL = process.env.DATABASE_URL;
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
-app.use('/', indexRouter)
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }))
+
+app.use('/', indexRouter);
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 mongoose.connect(MONGOURL, {
     family: 4
@@ -25,7 +33,7 @@ mongoose.connect(MONGOURL, {
     console.log("Database is connected successfully.");
 
     app.listen(PORT, () => {
-        console.log(`Server is runng on on ${PORT}`);
+        console.log(`Server is running on on ${PORT}`);
     });
 })
 .catch((error) => console.log(error))
